2ce18c222ca455942deead3ae7df6a0a4ee11d19
Petr Kolacek <xkolac11@stud.fit.vutbr.cz>
2012-11-28 22:33:14 +0100

    made skelet of class project()

---
+++ bin/iterator.py
@@ -0,0 +1,24 @@
+class project:
+
+
+	def __init__(self):
+	
+	# end of __init__
+	
+
+	def set_search_params(""" params """):
+	
+	# end of set_search_params
+	
+
+	def get_search_params(""" params """):
+	
+	# end of get_search_params
+	
+
+	def find_all_projects(""" params """):
+	
+	#end of find_all_projects
+
+
+# end of class project
